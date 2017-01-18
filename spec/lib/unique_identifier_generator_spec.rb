7c3143c158fb29971dfe791601aa9d90446ce734
Josh McMillan <josh@joshmcmillan.co.uk>
2017-01-18 15:19:35 +0000

    Refactor UniqueIdentifierGenerator specs
    This refactors the UniqueIdentifierGenerator specs to:
    - Not use "should" (http://betterspecs.org/#should)
    - Use contexts (http://betterspecs.org/#contexts)
    - Use subject (http://betterspecs.org/#subject)
    - Aggregate failures where appropriate

--- spec/lib/unique_identifier_generator_spec.rb
+++ spec/lib/unique_identifier_generator_spec.rb
@@ -1,56 +1,68 @@
 require 'unique_identifier_generator'
 
 RSpec.describe UniqueIdentifierGenerator do
-  let(:generator) { described_class.new }
+  describe '#create' do
+    it 'lower-cases the text' do
+      expect(subject.create('Heading', 1)).to eq "heading"
+    end
 
-  it 'should lower-case the text' do
-    expect(generator.create('Heading', 1)).to eq "heading"
-  end
+    it 'strips leading and trailing spaces' do
+      expect(subject.create('  heading  ', 1)).to eq "heading"
+    end
 
-  it 'should strip leading and trailing spaces' do
-    expect(generator.create('  heading  ', 1)).to eq "heading"
-  end
+    it 'replaces non alphanumeric characters with dashes' do
+      expect(subject.create('Headings (Etc) Are Great', 1)).to eq "headings-etc-are-great"
+    end
 
-  it 'should replace non alphanumeric characters with dashes' do
-    expect(generator.create('Headings (Etc) Are Great', 1)).to eq "headings-etc-are-great"
-  end
+    it 'does not add dashes at the start or end of the identifier' do
+      expect(subject.create('(Not (More) Headings!)', 1)).to eq "not-more-headings"
+    end
 
-  it 'should not include dashes at the start or end of the identifier' do
-    expect(generator.create('(Not (More) Headings!)', 1)).to eq "not-more-headings"
-  end
+    it 'strips HTML tags' do
+      expect(subject.create('Using <code>bash</code>', 1)).to eq "using-bash"
+    end
 
-  it 'should strip HTML tags' do
-    expect(generator.create('Using <code>bash</code>', 1)).to eq "using-bash"
-  end
+    context 'with a parent that is unique' do
+      it 'generates unique headings by prefixing with the parent' do
+        subject.create('Parent 1', 1)
+        first_heading = subject.create('The Same Heading', 2)
 
-  it 'should generate unique headings by prefixing with the parent' do
-    generator.create('Parent 1', 1)
-    first_heading = generator.create('The Same Heading', 2)
+        subject.create('Parent 2', 1)
+        second_heading = subject.create('The Same Heading', 2)
 
-    generator.create('Parent 2', 1)
-    second_heading = generator.create('The Same Heading', 2)
+        aggregate_failures do
+          expect(first_heading).to eq "the-same-heading"
+          expect(second_heading).to eq "parent-2-the-same-heading"
+        end
+      end
+    end
 
-    expect(first_heading).to eq "the-same-heading"
-    expect(second_heading).to eq "parent-2-the-same-heading"
-  end
+    context 'with a parent that is identical' do
+      it 'generates unique headings by suffixing with a number' do
+        subject.create('The Same Parent', 1)
 
-  it 'should generate unique headings by suffixing with a number if parent is identical' do
-    generator.create('The Same Parent', 1)
+        first_heading = subject.create('The Same Heading', 2)
+        second_heading = subject.create('The Same Heading', 2)
+        third_heading = subject.create('The Same Heading', 2)
 
-    first_heading = generator.create('The Same Heading', 2)
-    second_heading = generator.create('The Same Heading', 2)
-    third_heading = generator.create('The Same Heading', 2)
+        aggregate_failures do
+          expect(first_heading).to eq "the-same-heading"
+          expect(second_heading).to eq "the-same-parent-the-same-heading"
+          expect(third_heading).to eq "the-same-parent-the-same-heading-2"
+        end
+      end
+    end
 
-    expect(first_heading).to eq "the-same-heading"
-    expect(second_heading).to eq "the-same-parent-the-same-heading"
-    expect(third_heading).to eq "the-same-parent-the-same-heading-2"
-  end
+    context 'with no parent' do
+      it 'generates unique headings by suffixing with a number' do
+        first_heading = subject.create('The Same Heading', 1)
+        second_heading = subject.create('The Same Heading', 1)
 
-  it 'should generate unique headings by suffixing top level headings with a number' do
-    first_heading = generator.create('The Same Heading', 1)
-    second_heading = generator.create('The Same Heading', 1)
-
-    expect(first_heading).to eq "the-same-heading"
-    expect(second_heading).to eq "the-same-heading-2"
+        aggregate_failures do
+          expect(first_heading).to eq "the-same-heading"
+          expect(second_heading).to eq "the-same-heading-2"
+        end
+      end
+    end
   end
 end
