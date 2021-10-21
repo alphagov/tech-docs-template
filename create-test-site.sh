05cdc120ec1ca20c978ed300f77676578dcc497d
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2021-10-21 09:42:26 +0100

    Refactor create-test-site.sh
    - Use bundler to run middleman instead of installing it globally
    - Use shell `-e` flag to exit on errors, instead of relying on `&&`
    - Use shell `-x` flag to print commands, to aid with debugging failed
    runs

--- create-test-site.sh
+++ create-test-site.sh
@@ -1,3 +1,5 @@
+set -xe
+
 # This script is run as a pull request check. It generates a brand new site
 # from the template so that we can be confident that the project works out of
 # the box.
@@ -9,8 +11,7 @@ export USE_PAAS=true
 export APPLICATION_NAME=larry-the-cat
 export CANONICAL_HOST=larry-the-cat.cloudapps.digital
 
-gem install middleman &&
-middleman init tmp/test-run -T file://$(pwd) &&
-cd tmp/test-run &&
-bundle install &&
+bundle exec middleman init tmp/test-run -T file://$(pwd)
+cd tmp/test-run
+bundle install
 bundle exec middleman build --verbose
