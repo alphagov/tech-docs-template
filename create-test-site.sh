61ddeb498e90092ad3200d793d3ab2d2d564ac77
CarrJ <james.carr@dsit.gov.uk>
2026-08-25 14:28:37 +0100

    - update test job variables

--- create-test-site.sh
+++ create-test-site.sh
@@ -6,11 +6,11 @@ set -xe
 
 # The `middleman init` script can be configured with environment variables
 # instead of the interactive prompt.
-#export FIRST_TIME=true
-#export USE_GOVUK_LOGO=true
-#export SERVICE_NAME=example service
-#export SERVICE_URL=example-service.cloudapps.digital
-#export DOCUMENTATION_URL=example.docs
+export FIRST_TIME=true
+export USE_GOVUK_LOGO=true
+export SERVICE_NAME=example service
+export SERVICE_URL=example-service.cloudapps.digital
+export DOCUMENTATION_URL=example.docs
 
 bundle exec middleman init tmp/test-run -T file://$(pwd)
 cd tmp/test-run
